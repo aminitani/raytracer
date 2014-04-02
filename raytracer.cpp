1e7ddcef8e08d8e0b8bd8a54ef5d7d48b1d4d8d7
amin <aminoitani@gmail.com>
2014-04-02 18:33:41 -0400

    quick push

--- raytracer.cpp
+++ raytracer.cpp
@@ -19,6 +19,45 @@ int main()
 	
 	//trace some rays///////////////////////////////////////
 	
+	for (int j = 0; j < image.Height(); ++j) {
+		for (int i = 0; i < image.Width(); ++i) {
+			// compute primary ray direction
+//			Ray primRay;
+//			computePrimRay(i, j, &primRay);
+			// shoot prim ray in the scene and search for intersection
+//			Point pHit;
+//			Normal nHit;
+//			float minDist = INFINITY;
+//			Object object = NULL;
+//			for (int k = 0; k < objects.size(); ++k) {
+//				if (Intersect(objects[k], primRay, &pHit, &nHit)) {
+//					float distance = Distance(eyePosition, pHit);
+//					if (distance < minDistance) {
+//						object = objects[k];
+//						minDistance = distance; // update min distance
+//					}
+//				}
+//			}
+//			if (object != NULL) {
+				// compute illumination
+//				Ray shadowRay;
+//				shadowRay.direction = lightPosition - pHit;
+//				bool isShadow = false;
+//				for (int k = 0; k < objects.size(); ++k) {
+//					if (Intersect(objects[k], shadowRay)) {
+//						isInShadow = true;
+//						break;
+//					}
+//				}
+//			}
+//			if (!isInShadow)
+//				pixels[i][j] = object->color * light.brightness;
+//			else
+//				pixels[i][j] = 0;
+		}
+	}
+
+	//sample image generation
 	for(unsigned y = 0; y < image.Height(); y++)
 	for(unsigned x = 0; x < image.Width(); x++)
 	{
